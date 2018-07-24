5513a72e1d1f6a883bc1564f62d6cee677d39549
mizzick <ds.kolyshev@gmail.com>
2018-07-24 23:33:55 +0530

    filters

--- src/main/lib/filters-manager.js
+++ src/main/lib/filters-manager.js
@@ -3,19 +3,31 @@ const subscriptions = require('./filters/subscriptions');
 /**
  * Filters manager
  */
-module.exports = (function () {
+module.exports = (() => {
 
     const getFilters = () => {
         return subscriptions.getFilters();
     };
 
-    const isFilterEnabled = () => {
-        //TODO: Implement
-        return true;
+    /**
+     * Checks if specified filter is enabled
+     *
+     * @param filterId Filter identifier
+     * @returns {*} true if enabled
+     */
+    const isFilterEnabled = (filterId) => {
+        var filter = subscriptions.getFilter(filterId);
+        return filter && filter.enabled;
     };
 
+    /**
+     * Request Filter info
+     *
+     * @returns {{rulesCount: number}}
+     */
     const getRequestFilterInfo = () => {
         //TODO: Implement
+
         return {
             rulesCount: 0
         };
