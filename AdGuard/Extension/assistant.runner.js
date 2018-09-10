219cc2558eb6082555d03df9176c1ecd969f21b1
Roman Sokolov <still2.mail@me.com>
2018-09-10 21:38:40 +0300

    some fixes

--- AdGuard/Extension/assistant.runner.js
+++ AdGuard/Extension/assistant.runner.js
@@ -1,9 +1,14 @@
 (function () {
- var node = document.currentScript;
- console.log("(AdGuard Ext) Stoping AdGuard Assistant.");
- adguardAssistant().close();
- console.log("(AdGuard Ext) Starting AdGuard Assistant.");
- adguardAssistant().start(null,
+    var node = document.currentScript;
+    if (window.AdguardAssistant) {
+        console.log("(AdGuard Ext) Stoping AdGuard Assistant.");
+        AdguardAssistant.close();
+    }
+    else {
+        window.AdguardAssistant = new adguardAssistant();
+    }
+    console.log("(AdGuard Ext) Starting AdGuard Assistant.");
+    window.AdguardAssistant.start(null,
                           (rule) =>{
                           console.log("(AdGuard Ext) AdGuard Assistant callback.");
                           var event = new CustomEvent("assistant.runner-response", {
