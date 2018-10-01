5eedfd338109c4ad25207a889002c8dc38008076
Tim Shenin <t.shenin@adguard.com>
2018-10-01 16:10:42 +0300

    Add menu in the top left corner to enable hotkeys in app

---
+++ ElectronMainApp/src/main/main-menu.controller.js
@@ -0,0 +1,67 @@
+const { app, Menu } = require('electron');
+const i18n = require('../utils/i18n');
+
+/**
+ * Module Menu
+ * Define structure of Main menu which appears in the left top corner
+ * 
+ * This menu allows users to use hotkeys for commands such as Copy/Paste/Select all and so on
+ */
+module.exports = (() => {
+    const template = [
+        { 
+            label: 'AdGuard',
+            submenu: [
+                {
+                    label: i18n.__('main_menu_edit.message'),
+                    submenu: [
+                        { 
+                            label: i18n.__('main_menu_undo.message'),
+                            accelerator: 'cmd+z',
+                            selector: 'undo:' 
+                        },
+                        {
+                            label: i18n.__('main_menu_cut.message'),
+                            accelerator: 'cmd+x', 
+                            selector: 'cut:' 
+                        },
+                        { 
+                            label: i18n.__('main_menu_copy.message'),
+                            accelerator: 'cmd+c',
+                            selector: 'copy:' 
+                        },
+                        {
+                            label: i18n.__('main_menu_paste.message'),
+                            accelerator: 'cmd+v',
+                            selector: 'paste:' 
+                        },
+                        {
+                            label: i18n.__('main_menu_select-all.message'),
+                            accelerator: 'cmd+a',
+                            selector: 'selectAll:' 
+                        }
+                    ]
+                },
+                { type: 'separator' },
+                {
+                    label: i18n.__('tray_menu_quit.message'),
+                    accelerator: 'cmd+q',
+                    click() { app.quit(); }
+                }
+            ]
+        }
+    ];
+
+    /**
+     * Initialization method
+     * Should be execute when the app is ready
+     */
+    function initMenu() {
+        Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    }
+
+    return { initMenu }
+})();
+
+
+
