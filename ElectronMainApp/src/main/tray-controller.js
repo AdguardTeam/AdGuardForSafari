4d697ece5d2107b4a0e578a33bbc9eb6b7e00169
Tim Shenin <t.shenin@adguard.com>
2018-10-22 13:14:47 +0300

    Update version of electron Small code style changes

--- ElectronMainApp/src/main/tray-controller.js
+++ ElectronMainApp/src/main/tray-controller.js
@@ -7,7 +7,7 @@ const antibanner = require('./app/antibanner');
 const events = require('./events');
 const storage = require('./app/storage/storage');
 
-const {app, shell, Tray, Menu} = require('electron');
+const { app, shell, Tray, Menu } = require('electron');
 
 /**
  * Tray controller.
@@ -29,23 +29,6 @@ module.exports = (() => {
 
     const isOpenAtLoginEnabled = () => {
         return app.getLoginItemSettings().openAtLogin;
-
-        // TODO: Fix
-        // Due to https://github.com/electron/electron/issues/10880
-        // by default 'Launch at login is disabled'
-
-        // if (storage.getItem(LAUNCH_AT_LOGIN_KEY)) {
-        //     return app.getLoginItemSettings().openAtLogin;
-        // } else {
-        //     storage.setItem(LAUNCH_AT_LOGIN_KEY, true);
-        //
-        //     //Set default true
-        //     app.setLoginItemSettings({
-        //         openAtLogin: true
-        //     });
-        //
-        //     return true;
-        // }
     };
 
     const imageFolder = appPack.resourcePath('/src/main/icons');
@@ -62,28 +45,33 @@ module.exports = (() => {
         const tray = new Tray(trayImageOff);
         tray.setPressedImage(trayImageOff);
 
-        const contextMenu = Menu.buildFromTemplate([{
-            label: i18n.__('tray_menu_about.message'), click: () => {
-                shell.openExternal('https://github.com/AdguardTeam/AdGuardForSafari');
+        const contextMenu = Menu.buildFromTemplate([
+            {
+                label: i18n.__('tray_menu_about.message'),
+                click: () => {
+                    shell.openExternal('https://github.com/AdguardTeam/AdGuardForSafari');
+                }
+            },
+            {
+                label: i18n.__('tray_menu_preferences.message'),
+                click: () => { showWindow(); }
+            },
+            {
+                label: i18n.__('tray_menu_check_updates.message'), 
+                click: onCheckFiltersUpdateClicked
+            },
+            { type: "separator" },
+            {
+                label: i18n.__('tray_menu_launch_at_startup.message'),
+                type: "checkbox",
+                checked: isLaunchAtLogin,
+                click: onLaunchAdguardAtLoginClicked
+            },
+            { type: "separator" },
+            {
+                label: i18n.__('tray_menu_quit.message'),
+                click: () => { app.quit(); }
             }
-        }, {
-            label: i18n.__('tray_menu_preferences.message'), click: () => {
-                showWindow();
-            }
-        }, {
-            label: i18n.__('tray_menu_check_updates.message'), click: onCheckFiltersUpdateClicked
-        }, {
-            type: "separator"
-        }, {
-            label: i18n.__('tray_menu_launch_at_startup.message'), type: "checkbox",
-            checked: isLaunchAtLogin, click: onLaunchAdguardAtLoginClicked
-        }, {
-            type: "separator"
-        }, {
-            label: i18n.__('tray_menu_quit.message'), click: () => {
-                app.quit();
-            }
-        }
         ]);
 
         tray.setContextMenu(contextMenu);
