6923be74e5d182c7f18436f8647bfe2dbcd12e40
Tim Shenin <t.shenin@adguard.com>
2018-10-24 19:46:50 +0300

    Rewrite language detection Get language from navigator

--- ElectronMainApp/src/main/ui/js/i18n/i18n.js
+++ ElectronMainApp/src/main/ui/js/i18n/i18n.js
@@ -13,17 +13,31 @@ i18n.configure({
  * Translate elements on document ready
  */
 document.addEventListener("DOMContentLoaded", () => {
+    const defaultLocale = 'en';
+    const navigatorLang = navigator.language || navigator.browserLanguage;
+    const currentLocale = navigatorLang.substr(0, 2);
+    const catalog = i18n.getCatalog(currentLocale)
+        ? i18n.getCatalog(currentLocale)
+        : i18n.getCatalog(defaultLocale);
+
     document.querySelectorAll("[i18n]").forEach(el => {
-        const message = i18n.__(el.getAttribute("i18n") + '.message');
+        const key = el.getAttribute("i18n");
+        const message = catalog[key] && catalog[key].message;
         I18nHelper.translateElement(el, message);
     });
     document.querySelectorAll("[i18n-plhr]").forEach(el => {
-        el.setAttribute("placeholder", i18n.__(el.getAttribute("i18n-plhr") + '.message'));
+        const key = el.getAttribute("i18n-plhr");
+        const message = catalog[key] && catalog[key].message;
+        el.setAttribute("placeholder", message);
     });
     document.querySelectorAll("[i18n-href]").forEach(el => {
-        el.setAttribute("href", i18n.__(el.getAttribute("i18n-href") + '.message'));
+        const key = el.getAttribute("i18n-href");
+        const message = catalog[key] && catalog[key].message;
+        el.setAttribute("href", message);
     });
     document.querySelectorAll("[i18n-title]").forEach(el => {
-        el.setAttribute("title", i18n.__(el.getAttribute("i18n-title") + '.message'));
+        const key = el.getAttribute("i18n-title");
+        const message = catalog[key] && catalog[key].message;
+        el.setAttribute("title", message);
     });
 });
