2aa6f1c7e7666445629b7e21f76d13a1c825d6e1
mizzick <ds.kolyshev@gmail.com>
2019-02-21 23:02:58 +0200

    #114 Advanced Blocking - removed UI

--- AdGuard/AdvancedBlocking/SafariExtensionHandler.swift
+++ AdGuard/AdvancedBlocking/SafariExtensionHandler.swift
@@ -24,19 +24,4 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
             }
         }
     }
-    
-    override func toolbarItemClicked(in window: SFSafariWindow) {
-        // This method will be called when your toolbar item is clicked.
-        NSLog("The extension's toolbar item was clicked")
-    }
-    
-    override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
-        // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
-        validationHandler(true, "")
-    }
-    
-    override func popoverViewController() -> SFSafariExtensionViewController {
-        return SafariExtensionViewController.shared
-    }
-
 }
