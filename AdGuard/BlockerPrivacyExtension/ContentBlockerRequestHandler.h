1798056d50c414d7dfb37e08849f77a3b85c9e96
mizzick <ds.kolyshev@gmail.com>
2019-07-25 22:41:42 +0530

    multicb cb extensions

---
+++ AdGuard/BlockerPrivacyExtension/ContentBlockerRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  ContentBlockerRequestHandler.h
+//  BlockerPrivacyExtension
+//
+//  Created by Dimitry Kolyshev on 25.07.2019.
+//  Copyright © 2019 Adguard Software Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContentBlockerRequestHandler : NSObject <NSExtensionRequestHandling>
+
+@end
