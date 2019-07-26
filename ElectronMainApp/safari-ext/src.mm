96baf4acb7ffef83366bd3583b856037623c1b27
Roman Sokolov <calm@adguard.com>
2019-07-26 11:57:20 +0300

    info

--- ElectronMainApp/safari-ext/src.mm
+++ ElectronMainApp/safari-ext/src.mm
@@ -183,11 +183,11 @@ NAN_METHOD(setContentBlockingJson) {
     if (msg.length() > 0) {
         data = [NSData dataWithBytes:*msg length:msg.length()];
  	}
-    DDLogCDebug(@"(setContentBlockingJson) Data parameter length: %lu", data.length);
+    DDLogCInfo(@"(setContentBlockingJson) Data parameter length: %lu", data.length);
 
 	Nan::Utf8String message (info[0]);
 	NSString *bundleId = [NSString stringWithCString:*message encoding:NSUTF8StringEncoding];
-    DDLogCDebug(@"(setContentBlockingJson) BundleId parameter: %@", bundleId);
+    DDLogCInfo(@"(setContentBlockingJson) BundleId parameter: %@", bundleId);
     
     Nan::Callback *cb = new Nan::Callback(info[2].As<Function>());
 
