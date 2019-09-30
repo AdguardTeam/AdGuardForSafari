1325e6859a890b8a8b74e8a3784701f54a04d46a
mizzick <ds.kolyshev@gmail.com>
2019-09-30 21:11:07 +0700

    #233 rules storage cache

--- ElectronMainApp/src/main/app/storage/rules-storage.js
+++ ElectronMainApp/src/main/app/storage/rules-storage.js
@@ -3,25 +3,38 @@ const store = new Store();
 
 /**
  * Filter rules storage implementation
- * TODO: Look for faster and better implementation
  */
 module.exports = (() => {
+
+    const cache = Object.create(null);
 
     const getKey = (path) => {
         return 'filter_' + path;
     };
 
     const read = (path, callback) => {
+        const cached = cache[path];
+        if (cached) {
+            callback(cached);
+            return;
+        }
+
         const lines = store.get(getKey(path));
+        cache[path] = lines;
+
         callback(lines);
     };
 
     const write = (path, data, callback) => {
+        cache[path] = data;
+
         store.set(getKey(path), data);
         callback();
     };
 
     const remove = (path, successCallback) => {
+        delete cache[path];
+
         store.delete(getKey(path));
         successCallback();
     };
