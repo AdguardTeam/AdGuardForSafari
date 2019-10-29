9592ecff2f9ab5565e747d3f253bc2fbc869c9a4
Roman Sokolov <calm@adguard.com>
2019-10-29 17:00:52 +0300

    refactoring

--- AdGuard/AdGuard Login Helper/main.m
+++ AdGuard/AdGuard Login Helper/main.m
@@ -10,7 +10,43 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
+        // Check if main app is already running; if yes, do nothing and terminate helper app
+        BOOL alreadyRunning = NO;
+        NSArray *running = [[NSWorkspace sharedWorkspace] runningApplications];
+        for (NSRunningApplication *app in running) {
+            if ([[app bundleIdentifier] isEqualToString:AG_BUNDLEID]) {
+                alreadyRunning = YES;
+                break;
+            }
+        }
+        
+        if (!alreadyRunning) {
+            
+            NSString *appPath = [[[[[[NSBundle mainBundle] bundlePath] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent]  stringByDeletingLastPathComponent] stringByDeletingLastPathComponent];
+            // get to the waaay top. Goes through LoginItems, Library, Contents, Applications
+            NSURL *url = [NSURL fileURLWithPath:appPath];
+            if (url == nil) {
+                NSLog(@"AdGuard For Safari Login Helper: Can't obtain URL for Main App.");
+            }
+            else {
+                NSLog(@"AdGuard For Safari Login Helper: Try laungh: %@", url);
+                
+                NSError *lError = nil;
+                NSRunningApplication *app = [[NSWorkspace sharedWorkspace] launchApplicationAtURL:url
+                                                                                          options:0
+                                                                                    configuration:@{NSWorkspaceLaunchConfigurationEnvironment:
+                                                                                                        @{@"LAUNCHED_AT_LOGIN": @"1"}}
+                                                                                            error:&lError];
+                if (app == nil && lError != nil) {
+                    NSLog(@"AdGuard For Safari Login Helper: Error occurs when running: %@", lError);
+                }
+                else {
+                    NSLog(@"AdGuard For Safari Login Helper: App \"%@\" launched.", url);
+                }
+            }
+        }
+        
     }
-    return NSApplicationMain(argc, argv);
+    
+    return 0;
 }
