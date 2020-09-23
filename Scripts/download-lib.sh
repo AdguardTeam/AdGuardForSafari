17b8e60825f03fc7c35f0b7fa1b5aec2a515f712
mizzick <ds.kolyshev@gmail.com>
2020-09-23 16:21:07 +0200

    feature:AG-3905 swift-converter lib

--- Scripts/download-lib.sh
+++ Scripts/download-lib.sh
@@ -1,2 +1,12 @@
-curl -L "https://github.com/AdguardTeam/SafariConverterLib/releases/latest/download/ConverterTool" > ./libs/ConverterTool
+# TODO: Change repo to AdguardTeam
+
+curl -L "https://github.com/Mizzick/SafariConverterLib/releases/latest/download/ConverterTool" > ./libs/ConverterTool
 chmod +x ./libs/ConverterTool
+
+LIB_VERSION=$(curl -L "https://api.github.com/repos/Mizzick/SafariConverterLib/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/')
+
+echo "{\"version\": \"$LIB_VERSION\"}" > ./libs/ConverterTool.json
+chmod 777 ./libs/ConverterTool.json
+
