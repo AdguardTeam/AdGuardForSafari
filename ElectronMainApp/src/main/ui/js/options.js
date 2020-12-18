ccf873208e7912a28164831d7b7a5e8d3081bf3b
tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
2020-12-19 00:07:16 +0300

    layout fixes

--- ElectronMainApp/src/main/ui/js/options.js
+++ ElectronMainApp/src/main/ui/js/options.js
@@ -2166,12 +2166,15 @@ PageController.prototype = {
 
         const onboardingLink = document.querySelector('#onboarding-link');
         const onboardingTooltip = document.querySelector('#onboarding-tooltip');
+        const onboardingPic = document.querySelector('#onboarding-pic');
         onboardingLink.addEventListener('click', (e) => {
             e.preventDefault();
             if (onboardingTooltip.classList.contains('onboarding__tooltip__visible')) {
                 onboardingTooltip.classList.remove('onboarding__tooltip__visible');
+                onboardingPic.classList.add('onboarding__pic__visible');
             } else {
                 onboardingTooltip.classList.add('onboarding__tooltip__visible');
+                onboardingPic.classList.remove('onboarding__pic__visible');
             }
         });
 
