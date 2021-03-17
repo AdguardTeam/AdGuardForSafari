d9d97ffa243c63244a49f51788860ebf59c66ab6
tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
2021-03-17 20:29:38 +0300

    refactored content-blockers

--- ElectronMainApp/src/main/ui/js/options.js
+++ ElectronMainApp/src/main/ui/js/options.js
@@ -12,6 +12,8 @@ const Utils = require('./js/utils/common-utils');
 const checkboxUtils = require('./js/utils/checkbox-utils');
 // eslint-disable-next-line import/no-unresolved
 const TopMenu = require('./js/top-menu');
+// eslint-disable-next-line import/no-unresolved
+const ContentBlockersScreen = require('./js/content-blockers');
 
 const ANIMATION_DELAY = 900;
 
@@ -1366,157 +1368,6 @@ const Settings = function () {
 };
 
 /**
- * Content blockers tab
- *
- * @returns {*}
- * @constructor
- */
-const ContentBlockersScreen = function (antiBannerFilters, userFilter, whitelist) {
-    'use strict';
-
-    /**
-     * Elements to extension bundles dictionary
-     *
-     * @type {{*}}
-     */
-    const extensionElements = {
-        'com.adguard.safari.AdGuard.BlockerExtension': 'cb_general',
-        'com.adguard.safari.AdGuard.BlockerPrivacy': 'cb_privacy',
-        'com.adguard.safari.AdGuard.BlockerSocial': 'cb_social',
-        'com.adguard.safari.AdGuard.BlockerSecurity': 'cb_security',
-        'com.adguard.safari.AdGuard.BlockerOther': 'cb_other',
-        'com.adguard.safari.AdGuard.BlockerCustom': 'cb_custom',
-    };
-
-    /**
-     * Extension element for bundle identifier
-     *
-     * @param bundleId
-     * @return {*}
-     */
-    const getExtensionElement = (bundleId) => {
-        const elementId = extensionElements[bundleId];
-        if (elementId) {
-            return document.getElementById(elementId);
-        }
-
-        return null;
-    };
-
-    /**
-     * Updates content blockers info
-     *
-     * @param info
-     */
-    const updateContentBlockers = (info) => {
-        for (const extensionId in info.extensions) {
-            const state = info.extensions[extensionId];
-
-            const element = getExtensionElement(extensionId);
-            if (element) {
-                const icon = element.querySelector('.extension-block-ico');
-                const warning = element.querySelector('.cb_warning');
-                const rulesCount = element.querySelector('.cb_rules_count');
-
-                icon.classList.remove('block-type__ico-info--load');
-
-                icon.classList.add(state ? 'block-type__ico-info--check' : 'block-type__ico-info--warning');
-                warning.style.display = state ? 'none' : 'flex';
-                warning.textContent = i18n.__('options_cb_disabled_warning.message');
-
-                rulesCount.style.display = state ? 'flex' : 'none';
-            }
-        }
-    };
-
-    /**
-     * Updates extension rules count
-     *
-     * @param bundleId
-     * @param info
-     * @param filtersInfo
-     */
-    const updateExtensionState = (bundleId, info, filtersInfo) => {
-        const element = getExtensionElement(bundleId);
-        if (element) {
-            if (info) {
-                const rulesInfoElement = element.querySelector('.cb_rules_count');
-                const icon = element.querySelector('.extension-block-ico');
-
-                if (info.overlimit) {
-                    icon.classList.add('block-type__ico-info--overlimit-warning');
-
-                    let textContent = i18n.__('options_cb_rules_overlimit_info.message', info.rulesCount);
-                    textContent = textContent.replace('$2', info.rulesCount - 50000);
-
-                    // rulesInfoElement.style.display = 'flex';
-                    rulesInfoElement.classList.add('cb_overlimit_warning');
-                    rulesInfoElement.innerHTML = textContent;
-                } else if (info.hasError) {
-                    icon.classList.add('block-type__ico-info--overlimit-warning');
-
-                    // rulesInfoElement.style.display = 'flex';
-                    rulesInfoElement.classList.add('cb_overlimit_warning');
-                    rulesInfoElement.textContent = i18n.__('options_cb_compilation_warning.message');
-                } else {
-                    icon.classList.remove('block-type__ico-info--overlimit-warning');
-
-                    // rulesInfoElement.style.display = 'flex';
-                    rulesInfoElement.classList.remove('cb_overlimit_warning');
-                    rulesInfoElement.textContent = i18n.__n('options_cb_rules_info.message', info.rulesCount);
-                }
-            }
-
-            if (filtersInfo) {
-                const filtersInfoElement = element.querySelector('.cb_filters_info');
-                filtersInfoElement.textContent = filtersInfo;
-            }
-        }
-    };
-
-    /**
-     * Sets loading state for extensions
-     */
-    const setLoading = () => {
-        const extensionsIcons = document.querySelectorAll('.extension-block-ico');
-        extensionsIcons.forEach((ext) => {
-            ext.classList.remove('block-type__ico-info--warning');
-            ext.classList.remove('block-type__ico-info--check');
-
-            ext.classList.add('block-type__ico-info--load');
-        });
-    };
-
-    /**
-     * Initialize
-     *
-     */
-    const init = () => {
-        ipcRenderer.on('getContentBlockersMetadataResponse', (e, response) => {
-            const userFilterEnabled = userSettings.values[userSettings.names.USERRULES_ENABLED]
-                && !userFilter.isUserFilterEmpty();
-            const allowlistEnabled = userSettings.values[userSettings.names.ALLOWLIST_ENABLED]
-                && !whitelist.isAllowlistEmpty();
-            for (const extension of response) {
-                const filtersInfo = antiBannerFilters.getFiltersInfo(
-                    extension.groupIds,
-                    userFilterEnabled,
-                    allowlistEnabled
-                );
-                updateExtensionState(extension.bundleId, extension.rulesInfo, filtersInfo);
-            }
-        });
-    };
-
-    return {
-        updateContentBlockers,
-        setLoading,
-        updateExtensionState,
-        init,
-    };
-};
-
-/**
  * Page controller
  *
  * @constructor
@@ -1765,7 +1616,12 @@ PageController.prototype = {
         this.antiBannerFilters.render();
 
         // Initialize Content blockers
-        this.contentBlockers = new ContentBlockersScreen(this.antiBannerFilters, this.userFilter, this.whiteListFilter);
+        this.contentBlockers = new ContentBlockersScreen(
+            this.antiBannerFilters,
+            this.userFilter,
+            this.whiteListFilter,
+            userSettings
+        );
         this.contentBlockers.init();
 
         document.querySelector('#about-version-placeholder')
