e5feffc9b6a514e88c88fc5172d745f7502b254e
Oleg Labudko <o.labudko@adguard.com>
2022-08-09 17:35:47 +0300

    Pull request: Fix toolbar report link
    Merge in EXTENSIONS/safari-app-extension from fix/toolbar-report-link to master
    
    Squashed commit of the following:
    
    commit 7ce742741161d08bc997e0a8834275e0ab860646
    Merge: c1d87726 c355b0d7
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Aug 9 16:22:00 2022 +0300
    
        Merge branch 'master' into fix/toolbar-report-link
    
    commit c1d87726e938e13fdf14041b6ef65544ee6e5789
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Aug 9 15:47:48 2022 +0300
    
        fixed tollbar report link

--- ElectronMainApp/src/main/toolbar-controller.js
+++ ElectronMainApp/src/main/toolbar-controller.js
@@ -9,7 +9,8 @@ const log = require('./app/utils/log');
 const app = require('./app/app');
 const localStorage = require('./app/storage/storage');
 
-const REPORT_SAFARI_URL = 'https://link.adtidy.org/forward.html?action=report&from=toolbar&app=safari_extension';
+// eslint-disable-next-line max-len
+const REPORT_SAFARI_URL = 'https://link.adtidy.org/forward.html?action=report&from=toolbar&app=safari_extension&product_type=Saf';
 
 /**
  * Addon toolbar controller.
