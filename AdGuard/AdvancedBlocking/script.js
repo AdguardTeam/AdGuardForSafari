0cc26d54ee7f267d6f9a9a917a61cedb1a60748a
Slava Leleka <v.leleka@adguard.com>
2022-11-08 16:14:22 +0300

    AG-16445 fix ExtendedCss v2 usage
    Squashed commit of the following:
    
    commit b2c95ad5756acc62cfa2dad281c46d53164a4b37
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Nov 3 19:01:36 2022 +0200
    
        update extended-css to v2.0.4
    
    commit 334c2ff25baa5785e68435af099e2ae30c423a7c
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Wed Nov 2 19:50:52 2022 +0200
    
        fix ExtendedCss v2 usage

--- AdGuard/AdvancedBlocking/script.js
+++ AdGuard/AdvancedBlocking/script.js
@@ -128,14 +128,14 @@
         }
 
         logMessage(verbose, `extended css length: ${extendedCss.length}`);
-        const extcss = new ExtendedCss({
+        const extCss = new ExtendedCss.ExtendedCss({
             styleSheet: extendedCss
                 .filter(s => s.length > 0)
                 .map(s => s.trim())
                 .map(s => s[s.length - 1] !== '}' ? `${s} {display:none!important;}` : s)
                 .join('\n')
         });
-        extcss.apply();
+        extCss.apply();
     };
 
     /**
